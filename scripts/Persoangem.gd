3264977832636c48e69da9656277139df1110eba
Daniel <maffezzolidaniel78@gmail.com>
2024-01-29 20:55:04 -0300

    Movimentação e colisão coerentes

---
+++ scripts/Persoangem.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+
+var curr_pos = [0,0]
+var velo = 16
+var continue_ = true
+var movimentos = []
+var u = true
+var d = true
+var r = true
+var l = true
+
+@export var pos = 1
+@onready var animator = $CalebAnimation
+func _ready():
+	$Sprite2D.texture = load("res://sprites/Caleb.png")
+	position = Vector2(0,0)
+func _input(event):	
+	if continue_ == true and pos == 1:
+		if Input.is_action_pressed("ui_right") and r:
+			curr_pos[0] += velo
+			animation("r")
+			tween()
+		elif Input.is_action_pressed("ui_left") and l:
+			curr_pos[0] -= velo
+			animation("l")
+			tween()
+		elif Input.is_action_pressed("ui_down") and d:
+			curr_pos[1] += velo
+			animation("d")
+			tween()
+		elif Input.is_action_pressed("ui_up") and u:
+			curr_pos[1] -= velo
+			animation("u")
+			tween()
+func tween():
+	var tween = create_tween()
+	tween.tween_property($".", "position", Vector2(curr_pos[0], curr_pos[1]), 0.5)
+	movimentos.append(Vector2(curr_pos[0], curr_pos[1]))
+	continue_ = false
+	await get_tree().create_timer(0.5).timeout
+	continue_ = true
+	
+func animation(direction):
+	animator.play("walk_" + direction)
+	await animator.animation_finished
+	animator.play("idle_" + direction)
+
+
+func U_obstacle(area):
+	u = false
+	print("u")
+func U_free(area):
+	u = true
+	print("uf")
+func D_obstacle(area):
+	d = false
+	print("d")
+func D_free(area):
+	d = true
+	print("df")
+func R_obstacle(area):
+	r = false
+	print("r")
+func R_free(area):
+	r = true
+	print("rf")
+func L_obstacle(area):
+	l = false
+	print("l")
+func L_free(area):
+	l = true
+	print("lf")
